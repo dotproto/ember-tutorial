55cc7d11c7f11a2ad5bd6e3313ffa5cb5b9f1b40
Simeon Vincent <svincent@gmail.com>
2014-03-18 08:30:06 -0700

    Render post in posts, properly route post URls, format post metadata

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@ App = Ember.Application.create();
 // Define all of the URLs for the application
 App.Router.map(function() {
 	this.resource('about');
-	this.resource('posts');
-	this.resource('post', { path: ':post_id' });
+	this.resource('posts', function() {
+		this.resource('post', { path: ':post_id' });
+	});
 });
 
 App.PostsRoute = Ember.Route.extend({
@@ -14,18 +15,50 @@ App.PostsRoute = Ember.Route.extend({
 	}
 });
 
+App.PostRoute = Ember.Route.extend({
+	model: function(params) {
+		return posts.findBy('id', params.post_id);
+	}
+});
+
+App.PostController = Ember.ObjectController.extend({
+	isEditing: false,
+	
+	actions: {
+		edit: function() {
+			this.set('isEditing', true);
+		},
+
+		doneEditing: function() {
+			this.set('isEditing', false);
+		}
+	}
+});
+
+// Handlebars helper
+Ember.Handlebars.helper('format-date', function(date) {
+	return moment(date).fromNow();
+});
+
+var showdown = new Showdown.converter();
+Ember.Handlebars.helper('format-markdown', function(input) {
+	return new Handlebars.SafeString(showdown.makeHtml(input));
+});
+
+
+// Demo data
 var posts = [{
 	id: '1',
 	title: 'Rails is omakase',
 	author: { name: 'd2h' },
-	date: new Date('12-27-2012'),
+	date: new Date('12/27/2012'),
 	excerpt: 'This is an excerpt of D2H\'s "Rails is omakase" post',
 	body: 'Rails is omakase. *For reals.*'
 }, {
 	id: '2',
 	title: 'The Parley Letter',
 	author: { name: 'd2h' },
-	date: new Date('12-24-2012'),
+	date: new Date('12/24/2012'),
 	excerpt: 'This is an excerpt of D2H\'s "The Parley Letter" post',
 	body: 'Totally a real post. Nothing fake here.'
 }]
